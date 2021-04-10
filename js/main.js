5e8c2790111b6810cddfe2ba690536163737b09c
rakib <rakibhossain3574@gmail.com>
2021-04-10 21:19:00 +0600

    re-write small device css

--- js/main.js
+++ js/main.js
@@ -18,49 +18,6 @@ $(window).on('scroll', function () {
 	}
 });
 
-
-
-// mainSlider
-function mainSlider() {
-	var BasicSlider = $('.slider-active');
-	BasicSlider.on('init', function (e, slick) {
-		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-		doAnimations($firstAnimatingElements);
-	});
-	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-		doAnimations($animatingElements);
-	});
-	BasicSlider.slick({
-		autoplay: false,
-		autoplaySpeed: 10000,
-		dots: false,
-		fade: true,
-		arrows: false,
-		responsive: [
-			{ breakpoint: 767, settings: { dots: false, arrows: false } }
-		]
-	});
-
-	function doAnimations(elements) {
-		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		elements.each(function () {
-			var $this = $(this);
-			var $animationDelay = $this.data('delay');
-			var $animationType = 'animated ' + $this.data('animation');
-			$this.css({
-				'animation-delay': $animationDelay,
-				'-webkit-animation-delay': $animationDelay
-			});
-			$this.addClass($animationType).one(animationEndEvents, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-}
-mainSlider();
-
-
 // owlCarousel
 $('.testimonial_active').owlCarousel({
     loop:true,
@@ -79,7 +36,7 @@ $('.testimonial_active').owlCarousel({
             items:1,
 			nav:false
         },
-        768:{
+        770:{
             items:2
         },
         1200:{
